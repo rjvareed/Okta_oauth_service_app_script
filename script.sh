415ddb2ff1e8c14d374f9f12c20a0f18e275eeac
Roshan Vareed <99996854+rjvareed@users.noreply.github.com>
2025-08-11 21:51:50 -0500

    Add files via upload

--- script.sh
+++ script.sh
@@ -7,7 +7,7 @@ echo -e "Getting access token at endpoint $okta_domain/oauth2/v1/token"
 response=$(curl --location --request POST "$okta_domain/oauth2/v1/token" --header "Accept: application/json" --header "Content-Type: application/x-www-form-urlencoded" --data-urlencode "grant_type=client_credentials" --data-urlencode "scope=okta.apps.read" --data-urlencode "client_assertion_type=urn:ietf:params:oauth:client-assertion-type:jwt-bearer" --data-urlencode "client_assertion=$jwt")
 echo -e "$response\n"
 access_token=$(echo $response | jq -r '.access_token')
-echo -e "\x1b\x5b\x33\x31\x6d""Access token=$access_token""\x1b\x5b\x30\x6d"
+echo -e "\x1b\x5b\x33\x31\x6d""Access token=$access_token""\x1b\x5b\x30\x6d\n"
 echo -e "Making final request\nAPI response:"
 curl -X GET "$okta_domain/api/v1/apps/" --header "Accept: application/json" --header "Content-Type: application/json" --header "Authorization: Bearer $access_token"
 echo
