066a0d082a0c1d78fb9bb9d2d650e0c981c90767
Roshan Vareed <rjvareed@protonmail.com>
2025-08-14 01:21:52 -0500

    More concise code

--- generate_jwt.py
+++ generate_jwt.py
@@ -1,6 +1,6 @@
 import json, time, uuid, jwt
 from pathlib import Path
-from jwt.algorithms import RSAAlgorithm, ECAlgorithm
+from jwt.algorithms import RSAAlgorithm
 
 cfg = json.loads(Path("okta.json").read_text())
 OKTA_DOMAIN = cfg["okta_domain"].rstrip("/")
@@ -12,17 +12,22 @@ token_url = f"{OKTA_DOMAIN}/oauth2/v1/token"
 jwk_text = Path(JWK_PATH).read_text()
 jwk = json.loads(jwk_text)
 
-alg = jwk.get("alg") or ("RS256" if jwk.get("kty") == "RSA"
-                         else {"P-256":"ES256","P-384":"ES384","P-521":"ES512"}.get(jwk.get("crv"), "ES256"))
-
-key_obj = (RSAAlgorithm if jwk["kty"] == "RSA" else ECAlgorithm).from_jwk(jwk_text)
+key_obj = RSAAlgorithm.from_jwk(jwk_text)
 
 now = int(time.time())
+
 claims = {
-    "iss": CLIENT_ID, "sub": CLIENT_ID, "aud": token_url,
-    "iat": now, "exp": now + 300, "jti": str(uuid.uuid4())
+    "iss": CLIENT_ID,
+    "sub": CLIENT_ID,
+    "aud": token_url,
+    "iat": now,
+    "exp": now + 300,
+    "jti": str(uuid.uuid4())
 }
-headers = {k:v for k,v in {"kid": jwk.get("kid")}.items() if v}
+headers = {
+    "alg":"RS256",
+    "kid":jwk.get("kid"),
+    "typ":"JWT"
+}
 
-print(jwt.encode(claims, key_obj, algorithm=alg, headers=headers))
-
+print(jwt.encode(claims, key_obj, algorithm="RS256", headers=headers))
--- script.sh
+++ script.sh
@@ -2,12 +2,14 @@
 okta_domain=$(jq -r .okta_domain okta.json)
 echo "Generating JWT"
 jwt=$(python3 generate_jwt.py)
-echo -e "\x1b\x5b\x33\x31\x6d""JWT=$jwt""\x1b\x5b\x30\x6d\n"
+echo -e "\e[31mJWT=$jwt\e[0m\n"
 echo -e "Getting access token at endpoint $okta_domain/oauth2/v1/token"
+echo -e "\e[32mcurl --location --request POST \"$okta_domain/oauth2/v1/token\" --header \"Accept: application/json\" --header \"Content-Type: application/x-www-form-urlencoded\" --data-urlencode \"grant_type=client_credentials\" --data-urlencode \"scope=okta.apps.read\" --data-urlencode \"client_assertion_type=urn:ietf:params:oauth:client-assertion-type:jwt-bearer\" --data-urlencode \"client_assertion=$jwt\"\e[0m"
 response=$(curl --location --request POST "$okta_domain/oauth2/v1/token" --header "Accept: application/json" --header "Content-Type: application/x-www-form-urlencoded" --data-urlencode "grant_type=client_credentials" --data-urlencode "scope=okta.apps.read" --data-urlencode "client_assertion_type=urn:ietf:params:oauth:client-assertion-type:jwt-bearer" --data-urlencode "client_assertion=$jwt")
-echo -e "$response\n"
+echo -e "\nResponse:\n$response\n"
 access_token=$(echo $response | jq -r '.access_token')
-echo -e "\x1b\x5b\x33\x31\x6d""Access token=$access_token""\x1b\x5b\x30\x6d\n"
-echo -e "Making final request\nAPI response:"
-curl -X GET "$okta_domain/api/v1/apps/" --header "Accept: application/json" --header "Content-Type: application/json" --header "Authorization: Bearer $access_token"
-echo
+echo -e "\e[31mAccess token=$access_token\e[0m\n"
+echo "Making final request"
+echo -e "\e[32mcurl -X GET \"$okta_domain/api/v1/apps/\" --header \"Accept: application/json\" --header \"Content-Type: application/json\" --header \"Authorization: Bearer $access_token\"\e[0m"
+api_response=$(curl -X GET "$okta_domain/api/v1/apps/" --header "Accept: application/json" --header "Content-Type: application/json" --header "Authorization: Bearer $access_token")
+echo -e "\nAPI response:\n$api_response"
